41f554fc914147c3277de8a1ad463728ada874e7
kabir <kabirraihan249@gmail.com>
2025-07-08 20:41:46 +0600

    Update settings to include template directory and modify home view to render HTML template

--- mysite/views.py
+++ mysite/views.py
@@ -1,5 +1,6 @@
 
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("ki re shala vlo tw")
+    return render(request, "home.html")
